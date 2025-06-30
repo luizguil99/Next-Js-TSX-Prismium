484f5dc2dba967475e78c8a6802292800885df10
luizguil99 <124329344+luizguil99@users.noreply.github.com>
2025-06-30 17:07:19 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
-    <div><Button>oi</Button></div>
-      
-    
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold text-center">
+        This is a next js template
+      </h1>
+    </div>
   );
 }
